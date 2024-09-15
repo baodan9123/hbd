13753f42ddae65422c2a45fa0a633bff37360c3b
baodan9123 <baodan9123@gmail.com>
2024-09-15 12:03:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+
+var config = {
+
+    texts: [
+        "15/9",     
+        "Chúc Mừng", 
+        "Sinh Nhật",
+        "Ngthaoflakey",
+        "a.k.a",
+        "Ngọc Thảo!!!",
+        "Chúc",
+        "được",
+        "bank 100 đô",
+        "from VN with love",
+        "<3"
+    
+       
+    ],
+    imgs: {
+        "<3": "./imgs/Baodz.jpg",
+        "Ngọc Thảo!!!": "./imgs/ngthao.jpg", // trong ngoặc là câu xuất hiện ảnh
+    },
+    desc: {
+        turn_on: "BẬT ĐÈN",
+        play: "PHÁT NHẠC",
+        bannar_coming: "TRANG TRÍ CHO SINH NHẬT",
+        balloons_flying: "THẢ BÓNG BAY",
+        cake_fadein: "BÁNH KEM",
+        light_candle: "THẮP NẾN",
+        wish_message: "CHÚC MỪNG SINH NHẬT",
+        story: "Lời Chúc",
+    }
+};
